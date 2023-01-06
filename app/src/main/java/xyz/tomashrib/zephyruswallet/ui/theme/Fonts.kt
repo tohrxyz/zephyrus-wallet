1eefacb8998479db567a0e9281751b803e139d22
tomashrib <tomas.hrib@proton.me>
2023-01-06 16:26:20 +0100

    added fonts

---
+++ app/src/main/java/xyz/tomashrib/zephyruswallet/ui/theme/Fonts.kt
@@ -0,0 +1,14 @@
+package xyz.tomashrib.zephyruswallet.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import xyz.tomashrib.zephyruswallet.R
+
+val sourceSans = FontFamily(
+    Font(R.font.source_sans_pro_regular, FontWeight.Normal),
+)
+
+val sourceSansSemiBold = FontFamily(
+    Font(R.font.source_sans_pro_semibold, FontWeight.SemiBold)
+)
