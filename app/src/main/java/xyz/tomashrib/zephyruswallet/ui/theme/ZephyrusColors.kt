78a57a18375465c6601658a191db9ba0ec7f657d
tomashrib <tomas.hrib@proton.me>
2023-01-06 16:39:19 +0100

    added colors

---
+++ app/src/main/java/xyz/tomashrib/zephyruswallet/ui/theme/ZephyrusColors.kt
@@ -0,0 +1,11 @@
+package xyz.tomashrib.zephyruswallet.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+object ZephyrusColors{
+    val bgColorBlack: Color = Color(0xFF1C1B1F)
+    val surfaceBlack: Color = Color(0xFF1C1B1F)
+    val lightPurplePrimary: Color = Color(0xFFD0BCFF)
+    val darkerPurpleOnPrimary: Color = Color(0xFF381E72)
+    val lightGreyContainer: Color = Color(0xFF4A4458)
+}
